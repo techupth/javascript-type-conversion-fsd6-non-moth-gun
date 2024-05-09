bff00d2b330d3f60ad337b97d61408586bb9a1e8
Kan <kanalovefootball@gmail.com>
2024-05-09 15:08:25 +0700

    Answer-JavaScript-Type-Conversion-Assignment

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,9 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
+depositAmountFromUser = Number(depositAmountFromUser);
+console.log(depositAmountFromUser);
+
 accountBalance = accountBalance + depositAmountFromUser;
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
